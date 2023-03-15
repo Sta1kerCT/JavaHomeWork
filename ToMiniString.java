371a6c2db02eddd1fa98728521cf643b7b238235
Sta1kerCT <124699271+Sta1kerCT@users.noreply.github.com>
2023-03-15 04:31:29 +0300

    GK classes

--- ToMiniString.java
+++ ToMiniString.java
@@ -1,5 +1,8 @@
 package expression;
 
+/**
+ * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
+ */
 public interface ToMiniString {
     default String toMiniString() {
         return toString();
